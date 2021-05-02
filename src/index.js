7e3701d2be7af61fe0cec3657731fcf6d7b1d8a3
Alexandra <arbeuerman@gmail.com>
2021-05-02 16:29:13 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,95 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//make a get request
+const toyUrl =  'http://localhost:3000/toys';
+let toys = [];
+const toyCollection = document.getElementById('toy-collection');
+document.addEventListener('DOMContentLoaded', loadAllToys);
+
+function loadAllToys()
+{
+  fetch(toyUrl)
+  .then(response => response.json())
+  .then((data) => {
+    toys = data;
+    displayToys();
+  });
+}
+
+function displayToys()
+{
+  toyCollection.innerHTML = '';
+  toys.forEach(displayToy);
+}
+
+function displayToy(toy)
+{
+  const toyDiv = document.createElement('div');
+  toyDiv.className = 'card';
+
+  const toyHeader = document.createElement('h2');
+  toyDiv.appendChild(toyHeader);
+  toyHeader.innerText = toy.name; 
+
+  const toyImage = document.createElement('img');
+  toyImage.src = toy.image;
+  toyImage.className = 'toy-avatar';
+  toyDiv.appendChild(toyImage);
+
+  const toyParagraph = document.createElement('p');
+  toyParagraph.innerText = toy.likes;
+  toyDiv.appendChild(toyParagraph);
+
+  const toyButton = document.createElement('button');
+  toyButton.className = 'like-btn';
+  toyButton.innerText = 'Like <3';
+  toyButton.addEventListener('click', () => updateToyLikes(toy))
+  toyDiv.appendChild(toyButton);
+
+  toyCollection.appendChild(toyDiv);
+}
+
+//create a new toy
+//add an event listener to the form
+const toyForm = document.querySelector('form.add-toy-form');
+toyForm.addEventListener('submit', submitNewToy);
+
+function submitNewToy(event)
+{
+  event.preventDefault();
+  const name = event.target.name.value;
+  const image = event.target.image.value;
+  const likes = 0;
+
+  const toyData = {name, image, likes}
+
+  fetch(toyUrl, {
+    method: 'POST',
+    headers : {
+      'Content-Type': 'application/json',
+      'Accepts': 'application/json'
+    },
+    body: JSON.stringify(toyData)
+  })
+  .then(response => response.json())
+  .then(loadAllToys);
+}
+
+// get toy button and add event listener
+function updateToyLikes(toy)
+{
+  const singleToyUrl = `${toyUrl}/${toy.id}`;
+  toy.likes ++; //don't forget to do that part that you are changing haha
+  fetch(singleToyUrl, {
+    method: 'PATCH',
+    headers : {
+      'Content-Type': 'application/json',
+      'Accepts': 'application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(response => response.json())
+  .then(() => loadAllToys())
+}
